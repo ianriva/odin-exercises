c823923a799e5f8e796af6c3f9e52f878197c109
Ian Riva <ianriva96.ir@gmail.com>
2020-02-18 20:37:14 -0300

    updated sketch

--- sketch/scripts.js
+++ sketch/scripts.js
@@ -1,4 +1,9 @@
-
-for (let i=0;;) {
+let howManySquares = 16;
+for (let i=0;i<256;i++) {
     const square = document.createElement("div");
+    square.setAttribute("class", "square");
+    document.getElementById('container').appendChild(square);
 }
+
+const square = document.querySelectorAll(".square");
+console.log(square);
